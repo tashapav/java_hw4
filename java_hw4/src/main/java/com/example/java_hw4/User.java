aac1b777a8c8c5240fc6353113c362b1e3c286cb
tashapav <np76863@gmail.com>
2023-05-03 15:04:32 +0300

    fix commit

--- java_hw4/src/main/java/com/example/java_hw4/User.java
+++ java_hw4/src/main/java/com/example/java_hw4/User.java
@@ -1,20 +1,5 @@
 package com.example.java_hw4;
 
-import lombok.Data;
 
-@Data
-public class User {
-    private Long id;
-    private String login;
-    private String name;
-    private String surname;
-    private Integer age;
-    public User(Long id, String login, String name, String surname, Integer age) {
-        this.id = id;
-        this.login = login;
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-    }
-
+public record User(Long id, String login, String name, String surname, Integer age) {
 }
--- java_hw4/src/main/java/com/example/java_hw4/request/RequestV1.java
+++ java_hw4/src/main/java/com/example/java_hw4/request/RequestV1.java
@@ -1,11 +1,12 @@
 package com.example.java_hw4.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class RequestV1 {
+    @Nullable
     private String like;
 }
